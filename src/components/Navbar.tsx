3de0549c676c2b60c34bfb9b21e051ab599c15df
dietortoise25 <dietortoise25@gmail.com>
2025-04-09 16:35:02 +0800

    change title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import ThemeControler from "./ThemeControler";
 
 function Navbar() {
     return <div className="navbar bg-base-100 shadow-sm flex justify-between">
-        <a className="btn btn-ghost text-xl">TodoList with DaisyUI</a>
+        <a className="btn btn-ghost text-xl">TodoList</a>
         <ThemeControler />
     </div>
 }
