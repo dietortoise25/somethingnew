5b35ef71405445be59cef7546295ba2443132418
dietortoise25 <dietortoise25@gmail.com>
2025-04-10 12:19:17 +0800

    fix css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import TodoWrapper from "./components/TodoWrapper";
 
 function App() {
-  return <div className="flex flex-col gap-2 mx-auto p-10 max-w-[800px]">
+  return <div className="flex flex-col gap-2 mx-auto p-3 max-w-[800px]">
     <Navbar />
     <TodoWrapper />
   </div>;
